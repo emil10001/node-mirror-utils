130629babed350b57abd0bc8eb1488a2059954ae
E John Feig <ejohn@feigdev.com>
2014-03-19 09:40:14 -0700

    first

---
+++ README.md
@@ -0,0 +1,6 @@
+NodeJS Mirror API Utils
+==========================
+
+This is a helper utility library for the Google Glass Mirror API. More documentation to come.
+
+This is incomplete and untested.
---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by ejf3 on 3/18/14.
+ */
+var Auth = require('./lib/auth_utils.js')
+    , Card = require('./lib/card_utils.js')
+    , Contacts = require('./lib/contacts_utils.js')
+    , Menu = require('./lib/menu_utils.js')
+    , Timeline = require('./lib/timeline_utils.js');
+
+
+exports.Auth = Auth;
+exports.Card = Card;
+exports.Contacts = Contacts;
+exports.Menu = Menu;
+exports.Timeline = Timeline;
